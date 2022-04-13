371dc985d13d413f394466c32141b83cbe5d3800
joanavrsilva <joanavrsilva@gmail.com>
2022-04-13 16:52:35 +0000

    Templates: Add allauth templates

--- italian_restaurant_website/views.py
+++ italian_restaurant_website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from django.shortcuts import render
 
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    """ A view to return the index page """
+
+    return render(request, 'italian_restaurant_website/index.html')
