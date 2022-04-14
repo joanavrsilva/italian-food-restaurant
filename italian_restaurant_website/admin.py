bf26691c590c5aec8dda626fdb88d53cbcc967dc
joanavrsilva <joanavrsilva@gmail.com>
2022-04-14 16:26:07 +0000

    Add restaurant model default values

--- italian_restaurant_website/admin.py
+++ italian_restaurant_website/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Restaurants
+from .models import Restaurant
 
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
--- italian_restaurant_website/models.py
+++ italian_restaurant_website/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-
 from cloudinary.models import CloudinaryField
+import datetime
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
-    opening_time = models.TimeField(auto_now=False, auto_now_add=False)
-    closing_time = models.TimeField(auto_now=False, auto_now_add=False)
-    menu = CloudinaryField('image', default='placeholder')
+    opening_time = models.TimeField(
+        auto_now=False, auto_now_add=False, default=datetime.time(11, 00, 00))
+    closing_time = models.TimeField(
+        auto_now=False, auto_now_add=False, default=datetime.time(23, 00, 00))
+    menu = CloudinaryField('image', default='placeholder', use_filename=True)
 
-    def __str__(self):
-        return self.name
+def __str__(self):
+    return self.name
