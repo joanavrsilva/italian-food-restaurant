6fa63b7b5fe30a4366f8f6fe513f4b39eb404cdb
joanavrsilva <joanavrsilva@gmail.com>
2022-04-14 15:03:52 +0000

    Add admin panel restaurant model

--- italian_restaurant_website/admin.py
+++ italian_restaurant_website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Restaurants
 
-# Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'opening_time', 'closing_time')
+    search_fields = ('name',)
