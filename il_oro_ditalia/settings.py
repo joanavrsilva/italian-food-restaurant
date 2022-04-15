2453f45a1073d16e568a4fd577f22c00457eea48
joanavrsilva <joanavrsilva@gmail.com>
2022-04-15 17:22:22 +0000

    Atualize packages

--- il_oro_ditalia/settings.py
+++ il_oro_ditalia/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = True
 
 ALLOWED_HOSTS = ["italian-restaurant-website.herokuapp.com", "localhost"]
 
